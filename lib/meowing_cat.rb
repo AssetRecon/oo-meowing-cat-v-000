44be744542790e3e268993a380cba9c9cdee7df4
Edsel Caprice <caprice@mail.usf.edu>
2018-06-23 23:31:30 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,4 @@
-## code your solution here.
+## code your solution here. 
 class Cat
 
 attr_accessor :name
@@ -6,6 +6,12 @@ attr_accessor :name
   def meow
     puts "meow!"
   end
-
+  
 
 end
+
+
+maru = cat.new
+maru.name = "Maru"
+maru.name
+maru.meow
